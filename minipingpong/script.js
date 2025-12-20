86e492aaadf15cb5e0b49a1936fd151a56844dc8
Koop Reynders <k.g.reynders@hva.nl>
2025-12-21 00:10:29 +0100

    outsie click & clean up js

--- minipingpong/script.js
+++ minipingpong/script.js
@@ -1,13 +1,13 @@
 console.log("Start the games!")
-//class="active"
+
 var main = document.querySelector('main')
 var round1 = document.querySelector('#round1')
 var round2 = document.querySelector('#round2')
 var round3 = document.querySelector('#round3')
 var current = ""
 
-//init add all hover and click events
-function init(){
+//init add all hover events
+function init() {
     round1.addEventListener("mouseover",hover)
     round1.addEventListener("mouseout",removeAllHovers)
     round2.addEventListener("mouseover",hover)
@@ -15,40 +15,48 @@ function init(){
     round3.addEventListener("mouseover",hover)
     round3.addEventListener("mouseout",removeAllHovers)
 
-    if(window.location.hash){
+    if(window.location.hash) {
         startRound()
     }
 }
 //remove all hover classes and add current
-function hover(){
+function hover() {
     removeAllHovers()
     main.classList.add(this.id+"-hover")
 }
-function removeAllHovers(){
+function removeAllHovers() {
     main.classList.remove("round1-hover")
     main.classList.remove("round2-hover")
     main.classList.remove("round3-hover")
 }
 //start round, add active class
-function startRound(){
+function startRound() {
     removeAllHovers() //remove hover classes
     removeEvents()  //remove all events
     current = window.location.hash.split('#')[1]
-    // console.log("current: " + current)
     main.classList.add(current+"-active") //add active class to main
     main.querySelector("section#"+current).querySelector("a[href$='"+current+"']").classList.add("hide") //hide start link
     main.querySelector("section#"+current).querySelector(".rules").classList.remove("hide") //show rules in active section
+
+    document.addEventListener("click",outsideClick)
 }
 //remove active class, call init
-function resetRound(){
+function resetRound() {
     main.classList.remove(current+"-active") //remove active class to main
     main.querySelector("section#"+current).querySelector("a[href$='"+current+"']").classList.remove("hide") //show start link
     main.querySelector("section#"+current).querySelector(".rules").classList.add("hide") //hide rules in active section
 
+    document.removeEventListener("click",outsideClick)
     init() //call init function to start over
 }
-//
-function removeEvents(){
+function outsideClick(){
+    var round = main.querySelector("section#"+current)
+    if (!round.contains(event.target)) {
+        window.location = "#"
+    }
+}
+// remove all events
+function removeEvents() {
     round1.removeEventListener("mouseover",hover)
     round1.removeEventListener("mouseout",removeAllHovers) 
     round2.removeEventListener("mouseover",hover)
@@ -58,21 +66,20 @@ function removeEvents(){
 }
 
 //hash: check window location to navigate to active round
-window.addEventListener("hashchange", function(){ 
+window.addEventListener("hashchange", function() {
     // console.log("hashchange: ",window.location.hash)
-    if(window.location.hash){
+    if(window.location.hash) {
         startRound()
-    }else{
+    }else {
         resetRound()
     }
-});
-
+})
 
 //Start script 
 init()
 
 
-/* Learned:
+/* Learnings:
 
 replace element with cone removes all listeners...
     round1.replaceWith(round1.cloneNode(true))
